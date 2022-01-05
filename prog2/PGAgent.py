75fe2a9f946ddcfc1f0d325cb957ec54d0616075
moshebeutel <moshebeutel@gmail.com>
2022-01-05 18:38:40 +0200

    maybe fo submission

--- prog2/PGAgent.py
+++ prog2/PGAgent.py
@@ -1,4 +1,7 @@
 import numpy as np
+from numpy import random
+
+eps = 0.00001
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
@@ -13,6 +16,7 @@ class PolicyGradientAgent(object):
         self.W = np.random.rand(self.num_features, self.num_actions) * 0.01
         self.b = np.ones((self.num_actions)) * 0.01
         self.lr = lr
+        self.last_probs = np.zeros((self.num_actions))
 
     def action_probability(self, state):
         '''
@@ -29,8 +33,17 @@ class PolicyGradientAgent(object):
         :param state: environment state
         :return: action
         '''
-        probs = self.action_probability(state)
-        return np.random.choice(self.num_actions, p=probs)
+        self.last_probs = self.action_probability(state)
+        assert  1- eps < np.sum(self.last_probs) < 1 + eps
+        assert self.last_probs.size == 2
+        assert self.num_actions == 2
+        self.last_probs[0] = 0.0 if self.last_probs[0] < eps else self.last_probs[0]
+        self.last_probs[1] = 1.0 - self.last_probs[0]
+        self.last_probs[1] = 0.0 if self.last_probs[1] < eps else self.last_probs[1]
+        self.last_probs[0] = 1.0 - self.last_probs[1]
+        r = random.rand()
+        # return 0 if r < self.last_probs[0] else 1
+        return np.random.choice(self.num_actions, p=self.last_probs)
 
     def grad_log_prob(self, state, action):
         '''
@@ -40,9 +53,9 @@ class PolicyGradientAgent(object):
         :return: dlogP(a|s)/dW, dlogP(a|s)/db
         '''
         # TODO
-        probs = self.action_probability(state).reshape(-1,1)   # shape (num_actions,1) (2,1)
+        probs = self.last_probs.reshape(-1,1)   # shape (num_actions,1) (2,1)
         dsoftmax = (np.diagflat(probs) - np.dot(probs, probs.T))[action, :]  
-
+        assert probs[action,0] > eps
         dlogp_dz = (dsoftmax / probs[action,0]).reshape(1,-1)
         dz_dw = state.reshape(-1,1) # shape (num_features,)
         
@@ -50,6 +63,7 @@ class PolicyGradientAgent(object):
         db = np.zeros_like(self.b)
         dW[:,action] = (dz_dw @ dlogp_dz)[:,action]
         db[action] = dlogp_dz[:,action]  
+        assert not (np.isnan(dW).any() or np.isnan(db).any())
         return  dW, db
         # return  (dz_dw @ dlogp_dz)[:,action], dlogp_dz[:,action]  
 
@@ -60,9 +74,10 @@ class PolicyGradientAgent(object):
         :param dW: gradients w.r.t W
         :param db: gradients w.r.t b
         '''
+        assert not (np.isnan(dW).any() or np.isnan(db).any())
         self.W -= self.lr * dW
         self.b -= self.lr * db
-
+        assert not (np.isnan(self.W).any() or np.isnan(self.b).any())
         # print(f'update_weights dW = {dW} db = {db} self.lr * dW {self.lr * dW} self.lr * db {self.lr * db}')
 
 
--- prog2/train.py
+++ prog2/train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random.mtrand import rand
 from PGAgent import PolicyGradientAgent
 import gym
 import argparse
@@ -13,7 +14,7 @@ parser.add_argument('--lr',
 parser.add_argument('--N',
                     help='Number of updates',
                     type=int,
-                    default=4000)
+                    default=10000)
 parser.add_argument('--b',
                     help='batch size',
                     type=int,
@@ -28,37 +29,44 @@ parser.add_argument('--baseline',help='use baseline', dest='baseline', action='s
 
 args = parser.parse_args()
 np.random.seed(args.seed)
-gamma = 0.99
+gamma = 1.0
 env = gym.make('CartPole-v1')
 
 print('Enviourment: CartPole-v1 \nNumber of actions: ' ,env.action_space.n,'\nDimension of state space: ',np.prod(env.observation_space.shape))
-def run_episode(env, agent ,reward_to_go =False ,baseline=0., test_run=False):
+def run_episode(env, agent ,reward_to_go =False ,baseline=0, test_run=False):
     state = env.reset()
     rewards = []
     terminal = False
     episode_steps = 0
-    # dW = np.zeros_like(agent.W)
-    # db = np.zeros_like(agent.b)
     dw_actions, db_actions = [],[]
     
     while not terminal:
+        # if(test_run):
+        #     env.render()
         episode_steps += 1
         action = agent.get_action(state)
         state, reward, terminal, _ = env.step(action)
         rewards.append(reward)
-        #TODO fill in
-        dw_action, db_action = agent.grad_log_prob(state,action)
-        dw_actions.append(dw_action)
-        db_actions.append(db_action)
+        if not test_run:
+            dw_action, db_action = agent.grad_log_prob(state,action)
+            dw_actions.append(dw_action)
+            db_actions.append(db_action)
 
     if(test_run):
         return None, None, sum(rewards)
 
     discount_factors = gamma ** np.array(range(1,episode_steps + 1))
 
+    assert not (np.isnan(dw_actions).any() or np.isnan(db_actions).any())   
     if(baseline):
-        dw_baseline = np.multiply(np.square(dw_actions), np.array(rewards), axis=0).mean() / np.square(dw_actions).mean()
-        db_baseline = np.multiply(np.square(db_actions), np.array(rewards)).mean() / np.square(db_actions).mean()
+        sqr_w = np.square(dw_actions)
+        reward_like_w = np.array([np.ones_like(agent.W) * rewards[i] for i in range(episode_steps)]).reshape((episode_steps, *agent.W.shape))
+        dw_baseline =np.mean(np.multiply(sqr_w, reward_like_w),axis=0) / np.mean(sqr_w, axis=0)
+        dw_baseline[np.isnan(dw_baseline)] = 0.0
+        sqr_b = np.square(db_actions)
+        reward_like_b = np.array([np.ones_like(agent.b) * rewards[i] for i in range(episode_steps)]).reshape((episode_steps, *agent.b.shape))
+        db_baseline = np.mean(np.multiply(sqr_b, reward_like_b),axis=0) / np.mean(sqr_b, axis=0)
+        db_baseline[np.isnan(db_baseline)] = 0.0
     else:
         dw_baseline = 0.0
         db_baseline = 0.0
@@ -66,30 +74,34 @@ def run_episode(env, agent ,reward_to_go =False ,baseline=0., test_run=False):
     if(reward_to_go):
         for step in range(episode_steps):
             cummulated_reward_to_go =  np.array(rewards[step:]).T @ discount_factors[:episode_steps - step]
-            dw_actions[step] *= (cummulated_reward_to_go - dw_baseline)
-            db_actions[step] *= (cummulated_reward_to_go - db_baseline)
+            cummulated_reward_to_go_w = np.ones_like(dw_baseline) * cummulated_reward_to_go
+            dw_actions[step] *= (cummulated_reward_to_go_w - dw_baseline)
+            cummulated_reward_to_go_b = np.ones_like(db_baseline) * cummulated_reward_to_go
+            db_actions[step] *= (cummulated_reward_to_go_b - db_baseline)
         dW = np.sum(np.array(dw_actions).reshape(episode_steps, *agent.W.shape), axis=0)
         db = np.sum(np.array(db_actions).reshape(episode_steps, *agent.b.shape), axis=0)
     else:
         discounted_cummulated_reward = discount_factors.T @ np.array(rewards)
-        dW = np.sum(np.array(dw_actions).reshape(episode_steps, *agent.W.shape), axis=0) * (discounted_cummulated_reward - dw_baseline)
-        db = np.sum(np.array(db_actions).reshape(episode_steps, *agent.b.shape), axis=0) * (discounted_cummulated_reward - db_baseline)
-
+        discounted_cummulated_reward_w =  np.ones_like(dw_baseline) * discounted_cummulated_reward
+        dW = np.sum(np.array(dw_actions).reshape(episode_steps, *agent.W.shape), axis=0) * (discounted_cummulated_reward_w - dw_baseline)
+        discounted_cummulated_reward_b =  np.ones_like(db_baseline) * discounted_cummulated_reward
+        db = np.sum(np.array(db_actions).reshape(episode_steps, *agent.b.shape), axis=0) * (discounted_cummulated_reward_b - db_baseline)
+    assert not (np.isnan(dW).any() or np.isnan(db).any())  
     return dW , db , sum(rewards)
 
 
-def train(env, agent,args):
+def train(env, agent,N, b, rtg, baseline):
     rewards = []
-    for i in range(args.N):
+    for i in range(N // b):
         dW = np.zeros_like(agent.W)
         db = np.zeros_like(agent.b)
         rewards.append(0)
-        for j in range(args.b):
+        for j in range(b):
             #TODO fill in
-            episode_dW, episode_db, episode_rewards = run_episode(env,agent,reward_to_go=True, baseline=1)
-            dW += episode_dW / float(args.b)
-            db += episode_db / float(args.b)
-            rewards[i] += episode_rewards / float(args.b)
+            episode_dW, episode_db, episode_rewards = run_episode(env,agent,reward_to_go=rtg, baseline=baseline)
+            dW += episode_dW / float(b)
+            db += episode_db / float(b)
+            rewards[i] += episode_rewards / float(b)
         agent.update_weights(dW, db)
         if i%100 == 25:
             temp = np.array(rewards[i - 25:i])
@@ -101,20 +113,28 @@ def train(env, agent,args):
 def test(env, agent):
     rewards = []
     print('_________________________')
-    print('Running 1000 test episodes....')
-    for i in range(1000):
+    print('Running 500 test episodes....')
+    for i in range(500):
         _,_,r = run_episode(env,agent, test_run=True)
         rewards.append(r)
-    rewards = np.array(rewards)
-    rewards_to_plot = np.array([rewards[i - 200:i].mean() for i in range(200,1000)])
-    print('Test reward {:.1f}{}{:.1f}'.format(np.mean(rewards),u"\u00B1",np.std(rewards)/np.sqrt(1000.)))
-    return agent, rewards_to_plot
-    # return agent, rewards
+    print('Test reward {:.1f}{}{:.1f}'.format(np.mean(rewards),u"\u00B1",np.std(rewards)/np.sqrt(500.)))
+    return agent, rewards
 
+for b in [1, 5, 10]:
+    for rtg in [False, True]:
+        for baseline in [0,1]:
+            if not baseline or b > 1:
+                for lr in [0.0001,0.001]:
+                    agent = PolicyGradientAgent(env, lr=lr)
+                    agent, rewards = train(env,agent,args.N,b,rtg,baseline)
+                    print('Average training rewards: ',np.mean(rewards))
+                    _,test_rewards = test(env,agent)
+                    smoothed_train_rewards = np.array([np.mean(rewards[i - 20:i])for i in range(20,len(rewards))])
+                    str = f'learning rate {lr} batch size {b} baseline {baseline} reward_to_go {rtg} '
+                    plt.plot(smoothed_train_rewards)
+                    plt.savefig(str + f'avg train reward  {np.mean(rewards)}' +  '.png')
+                    plt.clf()
+                    plt.plot(test_rewards)
+                    plt.savefig(str + f'avg test reward  {np.mean(test_rewards)}' +  '.png')
+                    plt.clf()
 
-agent = PolicyGradientAgent(env, lr=args.lr)
-agent, rewards = train(env,agent,args)
-print('Average training rewards: ',np.mean(np.array(rewards)))
-test(env,agent)
-plt.plot(rewards)
-plt.show()
